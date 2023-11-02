3e272fd3411910f8a02186375e0c10feecf4bb07
nurmuhammad <mr.umarov98@gmail.com>
2023-11-02 22:24:54 +0900

    Updated

--- app/src/main/java/com/example/calculator_constraintlayout/MainActivity.java
+++ app/src/main/java/com/example/calculator_constraintlayout/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.calculator_constraintlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    String numbersText = "";
+    private TextView expressionTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        expressionTextView= (TextView) findViewById(R.id.expressionTextView);
+    }
+
+
+    @SuppressLint("NonConstantResourceId")
+    public void numberClicked (View view) {
+        numbersText += ((TextView) view).getText();
+
+        expressionTextView.setText(numbersText);
     }
 }
