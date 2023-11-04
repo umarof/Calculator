15763c70e072c6ea58988fc4d2a05e614c037742
nurmuhammad <mr.umarov98@gmail.com>
2023-11-04 10:55:24 +0900

    Updated

--- app/src/main/java/com/example/calculator_constraintlayout/MainActivity.java
+++ app/src/main/java/com/example/calculator_constraintlayout/MainActivity.java
@@ -8,22 +8,87 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    String numbersText = "";
-    private TextView expressionTextView;
+
+    String operator = "";
+
+    boolean isFirstNumber = true;
+
+    TextView taskTextView;
+
+    String number1 = "", number2 = "", taskString = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        taskTextView = findViewById(R.id.taskTextView);
 
-        expressionTextView= (TextView) findViewById(R.id.expressionTextView);
+
     }
 
 
-    @SuppressLint("NonConstantResourceId")
-    public void numberClicked (View view) {
-        numbersText += ((TextView) view).getText();
+    public void numberClicked(View view) {
 
-        expressionTextView.setText(numbersText);
+        String tempString = ((TextView) view).getText().toString();
+
+        if (isFirstNumber) {
+            number1 += tempString;
+        } else {
+            number2 += tempString;
+        }
+        taskString += tempString;
+        taskTextView.setText(taskString);
+
+    }
+
+    public void operatorClicked(View view) {
+        if (!isFirstNumber) return;
+
+        isFirstNumber = false;
+        operator = ((TextView) view).getText().toString();
+        taskString += operator;
+        taskTextView.setText(taskString);
+    }
+
+    public void clearClicked(View view) {
+        isFirstNumber = true;
+        taskString = "";
+        number1 = "";
+        number2 = "";
+        operator = "";
+        taskTextView.setText(taskString);
+
+    }
+
+
+    public void equalClick(View view) {
+        int num1 = Integer.parseInt(number1);
+        int num2 = Integer.parseInt(number2);
+        int total;
+
+        switch (operator) {
+            case "%":
+                total = num1 % num2;
+                break;
+            case "+":
+                total = num1 + num2;
+                break;
+            case "-":
+                total = num1 - num2;
+                break;
+            case "x":
+                total = num1 * num2;
+                break;
+            case "/":
+                total = num1 / num2;
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid Operator");
+        }
+
+        taskString += "=" + total;
+
+        taskTextView.setText(taskString);
+
     }
 }
